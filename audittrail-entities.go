2cdb8e9a91c9006db14e78224de589c0aed6fc8f
Muh. Nasrul <m.nasrul@qoin.id>
2023-11-12 20:27:32 +0700

    adjust rabbit command to new audittrail command

--- audittrail-entities.go
+++ audittrail-entities.go
@@ -1,8 +1,8 @@
 package qoinhubhelper
 
 const (
-	AUDITTRAIL_PROCESS = "audit-trial-process"
-	AUDITTRAIL_DATA    = "audit-trial-data"
+	AUDITTRAIL_PROCESS = "audit-trail-process"
+	AUDITTRAIL_DATA    = "audit-trail-data"
 	TIME_FORMAT        = "2006-01-02 15:04:05"
 )
 
