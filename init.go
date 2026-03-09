925a6ccb7ef097c01fe108ea5ecb79441e320216
Natan <natansdj@users.noreply.github.com>
2026-03-09 12:21:41 +0700

    update and fix env init

--- init.go
+++ init.go
@@ -2,6 +2,7 @@ package paycloudhelper
 
 import (
 	"os"
+	"path/filepath"
 
 	"bitbucket.org/paycloudid/paycloudhelper/phhelper"
 
@@ -14,9 +15,50 @@ func init() {
 	InitializeApp()
 }
 
+// findEnvPath returns a path to .env by trying, in order: ENV_FILE, CWD, parent dirs, executable dir.
+// Empty string means no .env was found. Used so apps find .env when run from IDE, subdirs, or different cwd.
+func findEnvPath() string {
+	if p := os.Getenv("ENV_FILE"); p != "" {
+		if _, err := os.Stat(p); err == nil {
+			return p
+		}
+	}
+	if wd, err := os.Getwd(); err == nil {
+		p := filepath.Join(wd, ".env")
+		if _, err := os.Stat(p); err == nil {
+			return p
+		}
+		dir := wd
+		for i := 0; i < 5; i++ {
+			dir = filepath.Dir(dir)
+			if dir == filepath.Dir(dir) {
+				break
+			}
+			p := filepath.Join(dir, ".env")
+			if _, err := os.Stat(p); err == nil {
+				return p
+			}
+		}
+	}
+	if exe, err := os.Executable(); err == nil {
+		p := filepath.Join(filepath.Dir(exe), ".env")
+		if _, err := os.Stat(p); err == nil {
+			return p
+		}
+	}
+	return ""
+}
+
 func InitializeApp() {
-	if err := godotenv.Load(); err != nil {
-		LogI("%s failed to load .env err=%v", buildLogPrefix("InitializeApp"), err)
+	envPath := findEnvPath()
+	if envPath != "" {
+		if err := godotenv.Load(envPath); err != nil {
+			LogD("%s failed to load .env from %s err=%v", buildLogPrefix("InitializeApp"), envPath, err)
+		}
+	} else {
+		if err := godotenv.Load(); err != nil {
+			LogD("%s .env not found (tried CWD and parents) err=%v", buildLogPrefix("InitializeApp"), err)
+		}
 	}
 
 	if appName := os.Getenv("APP_NAME"); appName != "" {
